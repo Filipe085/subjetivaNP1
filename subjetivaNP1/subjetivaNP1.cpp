ac38c7acd02bb708d1790ad25979138ced0c960f
Antonio Kauã <147512240+AntonioKaua@users.noreply.github.com>
2025-10-03 17:02:41 +0000

    calculadora base

--- subjetivaNP1/subjetivaNP1.cpp
+++ subjetivaNP1/subjetivaNP1.cpp
@@ -2,7 +2,24 @@
 using namespace std;
 
 int main(){
+        float vm, km_l, dist, vlr_comb, t, qtd_l, vlr_ab;
 
+        printf("Digite a velocidade media (km/h): ");
+        scanf("%f", &vm);
+        printf("Digite a autonomia (km/l): ");
+        scanf("%f", &km_l);
+        printf("Digite a distancia (km): ");
+        scanf("%f", &dist);
+        printf("Digite o valor do combustivel (R$/l): ");
+        scanf("%f", &vlr_comb);
+
+        t = dist/vm;
+        qtd_l = dist/km_l;
+        vlr_ab = qtd_l * vlr_comb;
+
+        printf("Tempo gasto na viagem: %.2f h\n", t);
+        printf("Quantidade de combustivel gasta: %.2fL\n", qtd_l);
+        printf("Valor gasto com combustivel: R$ %.2f\n", vlr_ab);
 
         return 0;
 }
